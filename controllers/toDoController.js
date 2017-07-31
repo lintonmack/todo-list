d8dfaf9752d46d2adc7b7625f4138199224aefff
Linton Mackereth <linton@Lintons-MBP.home>
2017-07-31 20:57:55 +0100

    Added todos

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,8 +1,17 @@
+var bodyParser = require('body-parser')
 var toDos = require('../models/ToDos.js')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 function toDoController (app) {
     app.get('/', function (req, res) {
-        console.log(toDos.getItems())
+        res.render('home', {
+            toDos: toDos.getItems()
+        })
+    }),
+    app.post('/', urlencodedParser, function (req, res) {
+        toDos.addItem(req.body.item)
+
         res.render('home', {
             toDos: toDos.getItems()
         })
